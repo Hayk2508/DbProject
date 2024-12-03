8ff954537bf44137a0451e68cbe4ef9adadc4c46
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-12-03 14:43:37 +0400

    Wrote command to run the initializing of tables

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table, TEXT, Date
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from models import Base
 
 POSTGRES_DB = "postgres"
 POSTGRES_USER = "root"
@@ -13,20 +13,11 @@ def get_connection():
         url=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{host}:{port}/{POSTGRES_DB}")
 
 
-Base = declarative_base()
 db = get_connection()
 
+Base.metadata.create_all(bind=get_connection())
 
-class GangMembers(Base):
-    __tablename__ = 'gang_members'
 
-    id = Column(Integer, primary_key=True)
-    nickname = Column(String(100), nullable=False)
-    status = Column(String(50))
-    contacts = Column(TEXT)
-    specialization = Column(String(100))
-    level = Column(Integer)
-    join_date = Column(Date)
 
 
 
